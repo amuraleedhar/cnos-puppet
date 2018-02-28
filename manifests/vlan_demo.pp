ef9909b4c47e552698d9d39f9f94438d87bf7c28
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-02-28 12:25:33 +0530

    Fixing Puppet-lint issues

--- manifests/vlan_demo.pp
+++ manifests/vlan_demo.pp
@@ -11,12 +11,13 @@
 
 class cnos::vlan_demo {
   cnos_vlan { '20':
-  ensure => 'present',
-  admin_state => 'up',
-  vlan_name => 'test20',}
+    ensure      => 'present',
+    admin_state => 'up',
+    vlan_name   => 'test20',}
 
   cnos_vlan { '21':
-  ensure => 'present',
-  admin_state => 'up',
-  vlan_name => 'test21',}
+    ensure      => 'present',
+    admin_state => 'up',
+    vlan_name   => 'test21',}
 }
+
