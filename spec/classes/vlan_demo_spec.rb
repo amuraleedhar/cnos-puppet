b40df6fb7caf9710faea436b5aca1faaf0a5ea30
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-01 19:04:38 +0530

    Update vlan_demo_spec.rb

--- spec/classes/vlan_demo_spec.rb
+++ spec/classes/vlan_demo_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe 'cnos::vlan_demo' do
-
+  it 'creates a vlan called vlan20' do
+    pp=<<-EOS
+    cnos_vlan { '20':
+      ensure                 => 'present',
+      vlan_name              => 'vlan20',
+      admin_state            => 'up',
+      vlan_id                => '20',
+    }
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
 end
