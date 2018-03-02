a373a276640df8a7174e2ff6bf49feebd7118011
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-02 18:40:08 +0530

    facts to fetch data from inventory

--- lib/puppet/util/network_device/cnos/facts.rb
+++ lib/puppet/util/network_device/cnos/facts.rb
@@ -18,7 +18,8 @@ class Puppet::Util::NetworkDevice::Cnos::Facts
     facts = {
       :operatingsystem => :cnos
     }
-    if response = @transport.call('/nos/api/system/')
+    #if response = @transport.call('/nos/api/system/')
+    if response = @transport.call('/nos/api/sysinfo/inventory')
       #result = items.first
       Puppet.debug("response  = #{response}")
       result = response
@@ -27,21 +28,45 @@ class Puppet::Util::NetworkDevice::Cnos::Facts
       return facts
     end
 
-    [ :switch_type,
-      :fw_version
+    #[ :switch_type,
+    #  :fw_version
+    #].each do |fact|
+    [ :Name,
+      :Description,
+      :Model,
+      :'Manufacture Date',
+      :'Serial Number',
+      :'Part Number',
+      :'Software Revision',
+      :'Electronic Serial Number',
+      :'Mgmt mac',
+      :'Machine Type Model',
+      :'UpTime',
     ].each do |fact|
       #Puppet.debug("response  = #{result[fact.to_s]}")
       facts[fact] = result[fact.to_s]
     end
 
     # Map CNOS names to expected standard names.
-    facts[:switch_type]            = facts[:switch_type]
-    facts[:fw_version]             = facts[:fw_version]
+    #facts[:switch_type]            = facts[:switch_type]
+    #facts[:fw_version]             = facts[:fw_version]
+    facts[:Name]                       = facts[:Name]
+    facts[:Description]                = facts[:Description]
+    facts[:Model]                      = facts[:Model]
+    facts[:'Manufacture Date']         = facts[:'Manufacture Date']
+    facts[:'Serial Number']            = facts[:'Serial Number']
+    facts[:'Part Number']              = facts[:'Part Number']
+    facts[:'Software Revision']        = facts[:'Software Revision']
+    facts[:'Serial Number']            = facts[:'Serial Number']
+    facts[:'Electronic Serial Number'] = facts[:'Electronic Serial Number']
+    facts[:'Mgmt mac']                 = facts[:'Mgmt mac']
+    facts[:'Machine Type Model']       = facts[:'Machine Type Model']
+    facts[:'UpTime']                   = facts[:'UpTime']
 
     facts.each do |key, value|
-       Puppet.debug("key  = #{key}")
-       Puppet.debug("value  = #{value}")
+       Puppet.notice("Key  = #{key} , Value = #{value}")
     end
+
     return facts
   end
 end
