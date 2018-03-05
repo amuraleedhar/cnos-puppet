cbe1bd8b6d9b3c8faf9b18accc32467beee4f8e2
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-05 11:49:50 +0530

    Create gem.rb

---
+++ lib/puppet/provider/cnos_vlag_isl/gem.rb
@@ -0,0 +1,36 @@
+# Copyright (c) 2017, Lenovo. All rights reserved.
+#
+# This program and the accompanying materials are licensed and made available
+# under the terms and conditions of the 3-clause BSD License that accompanies
+# this distribution. The full text of the license may be found at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
+# WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+require 'puppet/type'
+#require 'cnos-rbapi'
+#require 'cnos-rbapi/vlag'
+require File.join(File.dirname(__FILE__), '../cnos')
+require 'json'
+
+Puppet::Type.type(:cnos_vlag_isl).provide(:gem, parent: Puppet::Provider::Cnos) do
+  desc 'Manage Vlag on Lenovo CNOS. Requires cnos-rbapi'
+
+  #confine operatingsystem: [:ubuntu]
+
+  def port_aggregator
+    #conn = Connect.new('./config.yml')
+    #resp = Vlag.get_vlag_isl(conn)
+    resp = Puppet::Provider::Cnos.get_vlag_isl()
+    resp['port_aggregator']
+  end
+
+  def port_aggregator=(value)
+    #conn = Connect.new('./config.yml')
+    params = { 'port_aggregator' => resource[:port_aggregator] }
+    #resp = Vlag.update_vlag_isl(conn, params)
+    resp = Puppet::Provider::Cnos.update_vlag_isl(params)
+  end
+end
