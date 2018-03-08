b9f9eb18195b8e0c0468862e7d33f57d4630f978
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-08 17:18:37 +0530

    Latest Changes  7 Mar 2018

--- lib/puppet/type/cnos_vlan.rb
+++ lib/puppet/type/cnos_vlan.rb
@@ -12,7 +12,6 @@
 Puppet::Type.newtype(:cnos_vlan) do
   desc ' = {
             Manage Vlans on Lenovo cnos.
-
             Example:
              cnos_vlan {1:
                      vlan_name => vlan1,
@@ -23,29 +22,31 @@ Puppet::Type.newtype(:cnos_vlan) do
   ensurable
 
   # Parameters
-  #newproperty(:vlan_id, namevar: true) do
-  newparam(:vlan_id, namevar: true) do
-    desc 'vlan_id an integer from 2-3999'
+  newparam(:name, namevar: true) do
+    desc 'Name of VLAN.'
 
-=begin    munge do |value|
-      value.to_i
+    validate do |value|
+      super value
+      fail("the name must be between 1 and 64 characters long") if value.size > 64
     end
-=end
+  end
+
+  # Properties
+  newproperty(:vlan_id) do
+    desc 'vlan_id an integer from 1-3999'
+
     validate do |value|
       unless value.to_i.between?(1, 3999)
         fail "value not within limit (2-3999)"
       end
     end
    end
-
-  # Properties
+  
   newproperty(:vlan_name) do
- # newparam(:vlan_name) do
     desc 'string 32 characters long'
   end
 
   newproperty(:admin_state) do
-  #newparam(:admin_state) do
     desc 'one of up or down'
   end
 end
