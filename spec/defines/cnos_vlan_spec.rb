7dc161b394d73844f701b43f3368767d5346b7d1
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-08 17:29:47 +0530

    Updated on 7 March 2018

--- spec/defines/cnos_vlan_spec.rb
+++ spec/defines/cnos_vlan_spec.rb
@@ -1,25 +1,25 @@
-#require 'spec_helper'
-require 'rspec-puppet
+require 'spec_helper'
 
-type_class = Puppet::Type.type(:cnos_vlan)
+describe 'cnos_vlan', :type => :type do
+  let(:type_class) { Puppet::Type.type(:cnos_vlan) }
 
-describe type_class do
   let :params do
     [
-      :vlan_id,
+      :name,
     ]
   end
 
   let :properties do
-    %i[
-      :vlan_name
-      :admin_state
+    [
+      :vlan_id,
+      :vlan_name,
+      :admin_state,
     ]
   end
 
   it 'should have expected properties' do
     properties.each do |property|
-      expect(type_class.properties.map(&:vlan_id)).to be_include(property)
+      expect(type_class.properties.map(&:name)).to be_include(property)
     end
   end
 
@@ -27,15 +27,15 @@ describe type_class do
     params.each do |param|
       expect(type_class.parameters).to be_include(param)
     end
-  end
+end
 
-  it 'should require a vlan_id' do
+  it 'should require a name' do
     expect do
       type_class.new({})
     end.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
   it 'should support :present as a value to :ensure' do
-    type_class.new(vlan_id: '20', ensure: :present)
+    type_class.new(name: 'A great VLAN', vlan_id: '20', ensure: :present)
   end
 end
