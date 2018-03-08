a515ce6f0956b022e9f2f39ef51da8ae0dd28fc6
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-08 18:14:15 +0530

    pdk validate -a

--- lib/puppet/util/network_device/cnos/facts.rb
+++ lib/puppet/util/network_device/cnos/facts.rb
@@ -1,55 +1,53 @@
 class Puppet::Util::NetworkDevice::Cnos::Facts
-
   attr_reader :transport
 
   def initialize(transport)
-    Puppet.debug(self.class.to_s.split("::").last + ": Inside Initialize of Facts!")
+    Puppet.debug(self.class.to_s.split('::').last + ': Inside Initialize of Facts!')
     @transport = transport
   end
 
   def retrieve
-    Puppet.debug(self.class.to_s.split("::").last + ": Retrieving Facts from facts.rb!")
+    Puppet.debug(self.class.to_s.split('::').last + ': Retrieving Facts from facts.rb!')
     facts = {}
     facts.merge(parse_device_facts)
-    #facts = parse_device_facts()
+    # facts = parse_device_facts()
   end
 
   def parse_device_facts
     facts = {
-      :operatingsystem => :cnos
+      operatingsystem: :cnos
     }
-    #if response = @transport.call('/nos/api/system/')
+    # if response = @transport.call('/nos/api/system/')
     if response = @transport.call('/nos/api/sysinfo/inventory')
-      #result = items.first
+      # result = items.first
       Puppet.debug("response  = #{response}")
       result = response
     else
-      Puppet.warning("Did not receive device details. CNOS REST requires Administrator level access.")
+      Puppet.warning('Did not receive device details. CNOS REST requires Administrator level access.')
       return facts
     end
 
-    #[ :switch_type,
+    # [ :switch_type,
     #  :fw_version
-    #].each do |fact|
-    [ :Name,
-      :Description,
-      :Model,
-      :'Manufacture Date',
-      :'Serial Number',
-      :'Part Number',
-      :'Software Revision',
-      :'Electronic Serial Number',
-      :'Mgmt mac',
-      :'Machine Type Model',
-      :'UpTime',
-    ].each do |fact|
-      #Puppet.debug("response  = #{result[fact.to_s]}")
+    # ].each do |fact|
+    [:Name,
+     :Description,
+     :Model,
+     :'Manufacture Date',
+     :'Serial Number',
+     :'Part Number',
+     :'Software Revision',
+     :'Electronic Serial Number',
+     :'Mgmt mac',
+     :'Machine Type Model',
+     :UpTime].each do |fact|
+      # Puppet.debug("response  = #{result[fact.to_s]}")
       facts[fact] = result[fact.to_s]
     end
 
     # Map CNOS names to expected standard names.
-    #facts[:switch_type]            = facts[:switch_type]
-    #facts[:fw_version]             = facts[:fw_version]
+    # facts[:switch_type]            = facts[:switch_type]
+    # facts[:fw_version]             = facts[:fw_version]
     facts[:Name]                       = facts[:Name]
     facts[:Description]                = facts[:Description]
     facts[:Model]                      = facts[:Model]
@@ -61,12 +59,12 @@ class Puppet::Util::NetworkDevice::Cnos::Facts
     facts[:'Electronic Serial Number'] = facts[:'Electronic Serial Number']
     facts[:'Mgmt mac']                 = facts[:'Mgmt mac']
     facts[:'Machine Type Model']       = facts[:'Machine Type Model']
-    facts[:'UpTime']                   = facts[:'UpTime']
+    facts[:UpTime] = facts[:UpTime]
 
     facts.each do |key, value|
-       Puppet.notice("Key  = #{key} , Value = #{value}")
+      Puppet.notice("Key  = #{key} , Value = #{value}")
     end
 
-    return facts
+    facts
   end
 end
