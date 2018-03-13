0b46881156008c7ec85c6cfec2542d045095c00f
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-13 17:42:24 +0530

    PDK Validation fix

--- lib/puppet/type/cnos_arp.rb
+++ lib/puppet/type/cnos_arp.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2017, Lenovo. All rights reserved.
 #
 # This program and the accompanying materials are licensed and made available
-# under the terms and conditions of the 3-clause BSD License that accompanies 
+# under the terms and conditions of the 3-clause BSD License that accompanies
 # this distribution. The full text of the license may be found at
 #
 # https://opensource.org/licenses/BSD-3-Clause
@@ -27,8 +27,6 @@ Puppet::Type.newtype(:cnos_arp) do
   newproperty(:ageout_time) do
     desc 'integer from 60-28800'
 
-    munge do |value|
-      value.to_i
-    end
+    munge(&:to_i)
   end
 end
