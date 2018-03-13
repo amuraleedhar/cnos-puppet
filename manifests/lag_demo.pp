ae3baf06dca258bc11b6cd675c4ea675c7dfd19b
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-13 18:55:36 +0530

    PDK Validation fixes

--- manifests/lag_demo.pp
+++ manifests/lag_demo.pp
@@ -10,38 +10,38 @@
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 cnos_lag { '100':
-  ensure => 'present',
+  ensure       => 'present',
   interfaces   =>  [ {
-    "lag_mode" => "no_lacp",
-    "if_name"  => "Ethernet1/1"
+    'lag_mode' => 'no_lacp',
+    'if_name'  => 'Ethernet1/1'
   },
   {
-    "lag_mode" => "no_lacp",
-    "if_name"  => "Ethernet1/2"
+    'lag_mode' => 'no_lacp',
+    'if_name'  => 'Ethernet1/2'
   }],
   min_links    => 4,
 }
 
 cnos_lag { '20':
-  ensure => 'present',
+  ensure       => 'present',
   interfaces   =>  [ {
-    "lag_mode" => "no_lacp",
-    "if_name"  => "Ethernet1/9"
+    'lag_mode' => 'no_lacp',
+    'if_name'  => 'Ethernet1/9'
   },
   {
-    "lag_mode" => "no_lacp",
-    "if_name"  => "Ethernet1/10"
+    'lag_mode' => 'no_lacp',
+    'if_name'  => 'Ethernet1/10'
   }],
 }
 
 cnos_lag { '10':
-  ensure => 'present',
+  ensure       => 'present',
   interfaces   =>  [ {
-    "lag_mode" => "no_lacp",
-    "if_name"  => "Ethernet1/17"
+    'lag_mode' => 'no_lacp',
+    'if_name'  => 'Ethernet1/17'
   },
   {
-    "lag_mode" => "no_lacp",
-    "if_name"  => "Ethernet1/18"
+    'lag_mode' => 'no_lacp',
+    'if_name'  => 'Ethernet1/18'
   }],
 }
