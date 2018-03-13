18d2cbdca998b3dc9e52422df332e76fc2dab97f
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-13 18:12:14 +0530

    PDK Validations

--- lib/puppet/provider/cnos_arp_sys/gem.rb
+++ lib/puppet/provider/cnos_arp_sys/gem.rb
@@ -10,28 +10,28 @@
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 require 'puppet/type'
-#require 'cnos-rbapi'
-#require 'cnos-rbapi/arp'
+# require 'cnos-rbapi'
+# require 'cnos-rbapi/arp'
 require File.join(File.dirname(__FILE__), '../cnos')
 require 'json'
 
 Puppet::Type.type(:cnos_arp_sys).provide(:gem, parent: Puppet::Provider::Cnos) do
   desc 'Manage Arp_sys on Lenovo CNOS. Requires cnos-rbapi'
 
-  #confine operatingsystem: [:ubuntu]
+  # confine operatingsystem: [:ubuntu]
   mk_resource_methods
 
   def ageout_time
-    #conn = Connect.new('./config.yml')
-    #resp = Arp.get_arp_sys_prop(conn)
-    resp = Puppet::Provider::Cnos.get_arp_sys_prop()
+    # conn = Connect.new('./config.yml')
+    # resp = Arp.get_arp_sys_prop(conn)
+    resp = Puppet::Provider::Cnos.get_arp_sys_prop
     resp['ageout_time']
   end
 
-  def ageout_time=(value)
-    #conn = Connect.new('./config.yml')
+  def ageout_time=(_value)
+    # conn = Connect.new('./config.yml')
     params = { 'ageout_time' => resource[:ageout_time] }
-    #resp = Arp.set_arp_sys_prop(conn, params)
+    # resp = Arp.set_arp_sys_prop(conn, params)
     resp = Puppet::Provider::Cnos.set_arp_sys_prop(params)
   end
 end
