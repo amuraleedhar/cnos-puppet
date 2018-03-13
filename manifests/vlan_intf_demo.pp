92ef35b8c7ed39f79ef80e71cb4cb3750b58ea47
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-13 19:23:57 +0530

    Update vlan_intf_demo.pp

--- manifests/vlan_intf_demo.pp
+++ manifests/vlan_intf_demo.pp
@@ -13,15 +13,18 @@ class cnos::vlan_intf_demo {
   cnos_vlan_intf { 'po10':
     bridgeport_mode => 'trunk',
     pvid            => 1,
-    vlans           => [20,21],}
+    vlans           => [20,21],
+  }
 
   cnos_vlan_intf { 'po20':
     bridgeport_mode => 'trunk',
     pvid            => 1,
-    vlans           => [20,21],}
+    vlans           => [20,21],
+  }
 
   cnos_vlan_intf { 'po100':
     bridgeport_mode => 'trunk',
     pvid            => 1,
-    vlans           => [20,21],}
+    vlans           => [20,21],
+  }
 }
