d502d0019c57ced339069471e0c017faaf9cd7f5
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-13 18:11:06 +0530

    pdk validation changes

--- lib/puppet/provider/cnos_ip_intf/gem.rb
+++ lib/puppet/provider/cnos_ip_intf/gem.rb
@@ -10,28 +10,28 @@
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 require 'puppet/type'
-#require 'cnos-rbapi'
-#require 'cnos-rbapi/ip_intf'
+# require 'cnos-rbapi'
+# require 'cnos-rbapi/ip_intf'
 require File.join(File.dirname(__FILE__), '../cnos')
 require 'json'
 
 Puppet::Type.type(:cnos_ip_intf).provide(:gem, parent: Puppet::Provider::Cnos) do
   desc 'Manage IP interfaces on Lenovo CNOS. Requires cnos-rbapi'
 
-  #confine operatingsystem: [:ubuntu]
+  # confine operatingsystem: [:ubuntu]
 
   mk_resource_methods
 
   def self.instances
     instances = []
-    #conn = Connect.new('./config.yml')
-    #resp = Ipintf.get_ip_prop_all(conn)
-    resp = Puppet::Provider::Cnos.get_ip_prop_all()
-    return 'no ip_intf' if !resp
+    # conn = Connect.new('./config.yml')
+    # resp = Ipintf.get_ip_prop_all(conn)
+    resp = Puppet::Provider::Cnos.get_ip_prop_all
+    return 'no ip_intf' unless resp
     resp.each do |item|
-      Puppet.debug("Interface Name is "+ item['if_name'].to_s)
-      Puppet.debug("MTU is "+ item['mtu'].to_s)
-      Puppet.debug("IP Address is "+ item['ip_addr'].to_s)
+      Puppet.debug('Interface Name is ' + item['if_name'].to_s)
+      Puppet.debug('MTU is ' + item['mtu'].to_s)
+      Puppet.debug('IP Address is ' + item['ip_addr'].to_s)
       instances << new(name: item['if_name'],
                        vrf_name: item['vrf_name'],
                        bridge_port: item['bridge_port'],
@@ -39,13 +39,13 @@ Puppet::Type.type(:cnos_ip_intf).provide(:gem, parent: Puppet::Provider::Cnos) d
                        ip_addr: item['ip_addr'],
                        ip_prefix_len: item[:ip_prefix_len],
                        admin_state: item[:admin_state],
-                       ensure: :present,)
+                       ensure: :present)
     end
-    return instances
+    instances
   end
 
   def self.prefetch(resources)
-    Puppet.debug("I am inside prefetch")
+    Puppet.debug('I am inside prefetch')
     ip_intfs = instances
     resources.keys.each do |name|
       if provider = ip_intfs.find { |ip_intf| ip_intf.name == name }
@@ -56,11 +56,11 @@ Puppet::Type.type(:cnos_ip_intf).provide(:gem, parent: Puppet::Provider::Cnos) d
   end
 
   def flush
-    Puppet.debug("I am inside flush")
+    Puppet.debug('I am inside flush')
     if @property_hash
-      #conn = Connect.new('./config.yml')
+      # conn = Connect.new('./config.yml')
       params = params_setup
-      #resp = Ipintf.update_ip_prop_intf(conn, resource[:if_name], params)
+      # resp = Ipintf.update_ip_prop_intf(conn, resource[:if_name], params)
       resp = Puppet::Provider::Cnos.update_ip_prop_intf(resource[:if_name], params)
     end
     @property_hash = resource.to_hash
@@ -69,29 +69,23 @@ Puppet::Type.type(:cnos_ip_intf).provide(:gem, parent: Puppet::Provider::Cnos) d
   def params_setup
     params = {}
     params['if_name'] = resource[:if_name]
-    if resource[:bridge_port] != nil
+    unless resource[:bridge_port].nil?
       params['bridge_port'] = resource[:bridge_port]
     end
-    if resource[:mtu] != nil
-      params['mtu'] = resource[:mtu]
-    end
-    if resource[:ip_addr] != nil
-      params['ip_addr'] = resource[:ip_addr]
-    end
-    if resource[:ip_prefix_len] != nil
+    params['mtu'] = resource[:mtu] unless resource[:mtu].nil?
+    params['ip_addr'] = resource[:ip_addr] unless resource[:ip_addr].nil?
+    unless resource[:ip_prefix_len].nil?
       params['ip_prefix_len'] = resource[:ip_prefix_len]
     end
-    if resource[:vrf_name] != nil
-      params['vrf_name'] = resource[:vrf_name]
-    end
-    if resource[:admin_state] != nil
+    params['vrf_name'] = resource[:vrf_name] unless resource[:vrf_name].nil?
+    unless resource[:admin_state].nil?
       params['admin_state'] = resource[:admin_state]
     end
-    return params
+    params
   end
 
   def exists?
-    Puppet.debug("I am inside exists")
+    Puppet.debug('I am inside exists')
     @property_hash[:ensure] == :present
   end
 end
