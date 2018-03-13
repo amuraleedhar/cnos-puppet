2b9be88fbf61b063648aafc367c893dba6cc34c9
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-13 18:07:30 +0530

    pdk changes

--- lib/puppet/provider/cnos_sys/gem.rb
+++ lib/puppet/provider/cnos_sys/gem.rb
@@ -11,8 +11,8 @@
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 require 'puppet/type'
-#require 'cnos-rbapi'
-#require 'cnos-rbapi/telemetry'
+# require 'cnos-rbapi'
+# require 'cnos-rbapi/telemetry'
 require File.join(File.dirname(__FILE__), '../cnos')
 require 'json'
 
@@ -22,32 +22,32 @@ Puppet::Type.type(:cnos_sys).provide(:gem, parent: Puppet::Provider::Cnos) do
   mk_resource_methods
 
   def heartbeat_enable
-    #conn = Connect.new('./config.yml')
-    #resp = Telemetry.get_sys_feature(conn)
-    resp = Puppet::Provider::Cnos.get_sys_feature()
+    # conn = Connect.new('./config.yml')
+    # resp = Telemetry.get_sys_feature(conn)
+    resp = Puppet::Provider::Cnos.get_sys_feature
     resp['heartbeat-enable']
   end
 
   def msg_interval
-    #conn = Connect.new('./config.yml')
-    #resp = Telemetry.get_sys_feature(conn)
-    resp = Puppet::Provider::Cnos.get_sys_feature()
+    # conn = Connect.new('./config.yml')
+    # resp = Telemetry.get_sys_feature(conn)
+    resp = Puppet::Provider::Cnos.get_sys_feature
     resp['msg-interval']
   end
 
-  def heartbeat_enable=(value)
-    #conn = Connect.new('./config.yml')
+  def heartbeat_enable=(_value)
+    # conn = Connect.new('./config.yml')
     params = { 'heartbeat-enable' => resource[:heartbeat_enable],
                'msg-interval' => resource[:msg_interval] }
-    #resp = Telemetry.set_sys_feature(conn, params)
+    # resp = Telemetry.set_sys_feature(conn, params)
     resp = Puppet::Provider::Cnos.set_sys_feature(params)
   end
 
-  def msg_interval=(value)
-    #conn = Connect.new('./config.yml')
+  def msg_interval=(_value)
+    # conn = Connect.new('./config.yml')
     params = { 'heartbeat-enable' => resource[:heartbeat_enable],
                'msg-interval' => resource[:msg_interval] }
-    #resp = Telemetry.set_sys_feature(conn, params)
+    # resp = Telemetry.set_sys_feature(conn, params)
     resp = Puppet::Provider::Cnos.set_sys_feature(params)
   end
 end
