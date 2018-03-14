f652a43a668792c8c9d82aae689e4e1e5fa11ef8
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-14 19:48:18 +0530

    Update vlag_demo.pp

--- manifests/vlag_demo.pp
+++ manifests/vlag_demo.pp
@@ -11,19 +11,19 @@
 
 class cnos::vlag_demo {
   cnos_vlag_conf{'vlag_conf':
-    ensure => 'present',
-    tier_id => 10,
+    ensure          => present,
+    tier_id         => 10,
   }
   cnos_vlag_isl {'vlag_isl':
     port_aggregator => 100,
   }
   cnos_vlag { '1':
-    ensure          => 'present',
+    ensure          => present,
     status          => 'enable',
     port_aggregator => 10,
   }
   cnos_vlag { '2':
-    ensure          => 'present',
+    ensure          => present,
     status          => 'enable',
     port_aggregator => 20,
   }
