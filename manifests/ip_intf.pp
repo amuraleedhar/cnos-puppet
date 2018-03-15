0cf466f10603a7fe4d7466470a9f7569116b93a8
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-15 17:06:09 +0530

    Update ip_intf.pp

--- manifests/ip_intf.pp
+++ manifests/ip_intf.pp
@@ -10,11 +10,11 @@
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 class cnos::ip_intf {
-  cnos_ip_intf{'Ethernet1/1':
+  cnos_ip_intf{'Ethernet1/11':
     ensure        => present, 
     ip_addr       => '1.1.1.1',
     bridge_port   => 'no',
-    if_name       => 'Ethernet1/1',
+    if_name       => 'Ethernet1/11',
     mtu           => 1502,
     vrf_name      => 'default',
     admin_state   => 'up',
