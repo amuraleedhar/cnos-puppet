37b90e2516f4dc871d8dd0afd8a75f94c1956578
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-20 16:40:43 +0530

    validation errors fix

--- manifests/vlag_demo.pp
+++ manifests/vlag_demo.pp
@@ -11,8 +11,8 @@
 
 class cnos::vlag_demo {
   cnos_vlag_conf{'vlag_conf':
-    ensure          => present,
-    tier_id         => 10,
+    ensure  => present,
+    tier_id => 10,
   }
   cnos_vlag_isl {'vlag_isl':
     port_aggregator => 100,
