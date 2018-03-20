2b27b820487f8b4514e47bd994da9d162ef66d31
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-20 16:38:57 +0530

    Validation change merge

--- manifests/lag_demo.pp
+++ manifests/lag_demo.pp
@@ -11,8 +11,8 @@
 
 class cnos::lag_demo {
   cnos_lag { '100':
-    ensure       => present,
-    interfaces   => [ {
+    ensure     => present,
+    interfaces => [ {
       'lag_mode' => 'no_lacp',
       'if_name'  => 'Ethernet1/1',
     },
@@ -20,7 +20,7 @@ class cnos::lag_demo {
       'lag_mode' => 'no_lacp',
       'if_name'  => 'Ethernet1/2',
     }],
-    min_links    => 4,
+    min_links => 4,
   }
   cnos_lag { '20':
     ensure       => present,
@@ -34,8 +34,8 @@ class cnos::lag_demo {
     }],
   }
   cnos_lag { '10':
-    ensure       => present,
-    interfaces   => [ {
+    ensure     => present,
+    interfaces => [ {
       'lag_mode' => 'no_lacp',
       'if_name'  => 'Ethernet1/17',
     },
